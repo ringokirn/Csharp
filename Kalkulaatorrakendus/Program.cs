5fb6cd17a0aaaaf4ea01676f272228fec09a0f19
ringokirn <afakfjagagopj@gmail.com>
2025-05-20 16:40:15 +0300

    Add files via upload

---
+++ Kalkulaatorrakendus/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Sisesta esimene arv:");
+double arv1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Sisesta teine arv:");
+double arv2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Sisesta tehe (+, -, *, /):");
+char tehe = Convert.ToChar(Console.ReadLine());
+
+double tulemus = 0;
+
+if (tehe == '+')
+{
+    tulemus = arv1 + arv2;
+}
+else if (tehe == '-')
+{
+    tulemus = arv1 - arv2;
+}
+else if (tehe == '*')
+{
+    tulemus = arv1 * arv2;
+}
+else if (tehe == '/')
+{
+    tulemus = arv1 / arv2;
+}
+Console.WriteLine("Tulemus: " + tulemus);
